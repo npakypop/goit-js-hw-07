12eae0bfa677e13bbad38b8bccba8d3d56ae5261
npakypop <leaterood@gmail.com>
2022-11-06 00:16:34 +0200

    7 part 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryImg = createGallery(galleryItems);
+galleryRef.addEventListener('click', onGalleryItem);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryImg);
+
+
+function onGalleryItem(event) { 
+    event.preventDefault();
+
+    const imgSource = event.target.dataset.source;
+    if (!imgSource) { 
+        return;
+    }
+    // const lightbox = new SimpleLightbox('.gallery gallery__item', {});
+}
+
+function createGallery(items) { 
+    return items.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">  
+        <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+        </a>
+        </div>
+        `;
+    }).join('');
+};
 
 console.log(galleryItems);
